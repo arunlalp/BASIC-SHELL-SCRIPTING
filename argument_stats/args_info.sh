cc82a60d375b84f2ae507a5ff4f3918b37f8047f
Arun Lal <118952274+arunlal555@users.noreply.github.com>
2023-05-19 13:57:12 +0530

    args_info.sh

---
+++ argument_stats/args_info.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Display the total count of arguments
+echo "Total Count of Arguments: $#"
+
+# Display the value of the first argument
+echo "Value of the First Argument: $1"
+
+# Display the PID of the script
+echo "Script PID: $$"
+
+# Display all the supplied arguments
+echo "Supplied Arguments: $@"
